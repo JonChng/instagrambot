67e03e61c3b158e53cf02e6f4ba5a2aa11a52fd9
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-17 21:13:34 +0800

    env files added

--- main.py
+++ main.py
@@ -7,10 +7,36 @@ import time
 
 dotenv.load_dotenv()
 
+user = os.environ['LOGIN']
+password = os.environ['PASSWORD']
+print(user)
+print(password)
+
 chromedriver_path = "/Users/jonathanchng/Downloads/chromedriver_mac64/chromedriver"
 
 link = "https://www.instagram.com/"
 
 driver = webdriver.Chrome(chromedriver_path)
+driver.get(link)
 
+time.sleep(10)
+
+username = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+username.send_keys(user)
+
+word = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+word.send_keys(password)
+word.send_keys(Keys.ENTER)
+
+time.sleep(15)
+
+save = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/section/div/button')
+save.click()
+
+time.sleep(30)
+
+notif = driver.find_element(By.XPATH, '//*[@id="mount_0_0_90"]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]')
+notif.click()
+
+driver.quit()
 
